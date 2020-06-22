1debe2f755ea4fcc3f2fc2f407619fd980172e1b
Alon Emanuel <alon@alonemanuel.com>
2020-06-22 15:43:39 +0300

    rearranged dirs

--- data_processing/data_process_exception.py
+++
@@ -1,7 +0,0 @@
-class DataProcessException(Exception):
-    def __init__(self, subject):
-        self.message = f'Data processing exception, has to to with:\n{subject}'
-        super().__init__(self.message)
-
-    def __str__(self):
-        return self.message
